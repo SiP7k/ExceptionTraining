58e660bbc2732eb0322610c085dd56316dcdaf25
SiP <cavvach@gmail.com>
2022-08-31 10:32:13 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExeptionTraining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
