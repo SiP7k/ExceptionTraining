af4134f00f908d9ea9a9035ceb0c0a77db7851ce
SiP <cavvach@gmail.com>
2022-09-11 11:58:30 +0300

    HW1

---
+++ InputException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ExceptionTraining
+{
+    class InputException : Exception
+    {
+        public InputException(){}
+        public InputException(string message) : base(message){}
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 
-namespace ExeptionTraining
+namespace ExceptionTraining
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Exception[] exceptions = new Exception[] {new ArgumentException(), new IndexOutOfRangeException(),new InputException(),new DivideByZeroException(), new FormatException()};
+            
+            foreach (var exception in exceptions)
+            {
+                try
+                {
+                    throw exception;
+                }
+                catch (Exception ex)
+                {
+                    if (ex is InputException)
+                    {
+                        Console.WriteLine("Ошибка ввода!");
+                    }
+                    else
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
         }
     }
 }
